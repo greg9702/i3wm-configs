7f7401aba6aebb3cc1032a4c03b08ce6e7c173a5
g_broton <30444094+greg9702@users.noreply.github.com>
2018-10-21 11:16:03 +0200

    Added ss image as background

--- i3/i3exit.sh
+++ i3/i3exit.sh
@@ -1,7 +1,28 @@
 #!/bin/sh
 lock() {
     #i3lock -c 222222
-    i3lock -i ~/.config/i3/Wallpeper/lockscreen.png -t
+    IMAGE=/tmp/i3lock.png
+SCREENSHOT="scrot $IMAGE" # 0.46s
+
+# Alternate screenshot method with imagemagick. NOTE: it is much slower
+# SCREENSHOT="import -window root $IMAGE" # 1.35s
+
+# Here are some imagemagick blur types
+# Uncomment one to use, if you have multiple, the last one will be used
+
+# All options are here: http://www.imagemagick.org/Usage/blur/#blur_args
+#BLURTYPE="0x5" # 7.52s
+#BLURTYPE="0x2" # 4.39s
+#BLURTYPE="5x2" # 3.80s
+#BLURTYPE="2x3" # 2.92s
+BLURTYPE="0x3" # 2.50s
+
+# Get the screenshot, add the blur and lock the screen with it
+$SCREENSHOT
+convert $IMAGE -blur $BLURTYPE $IMAGE
+i3lock -i $IMAGE
+rm $IMAGE
+    #i3lock -i ~/.config/i3/Wallpeper/lockscreen.png -t
 }
 
 case "$1" in
