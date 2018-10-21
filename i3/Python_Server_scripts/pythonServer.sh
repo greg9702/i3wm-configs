81c339c8f860334827c469eb07b330a2756b29d8
g_broton <30444094+greg9702@users.noreply.github.com>
2018-10-21 19:49:32 +0200

    Update pythonServer.sh

--- i3/Python_Server_scripts/pythonServer.sh
+++ i3/Python_Server_scripts/pythonServer.sh
@@ -3,7 +3,7 @@
 status=$(python2 /home/greg9702/.config/i3/Python_Server_scripts/isup.py)
 if [ $status -eq 1 ] ; then
   touch /tmp/abc2
-    server_pid=$(top -b -n 1 | grep [p]ython$ | awk '{print $1}')
+    server_pid=$(top -b -n 1 | grep python$ | awk '{print $1}')
     kill -9 $server_pid
 else
     pushd /home/greg9702/Server; python -m SimpleHTTPServer 8000
